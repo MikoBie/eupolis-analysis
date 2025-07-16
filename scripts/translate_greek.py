33e41c8320833c09e00e8abadc8d32c14cbe5d68
MikoBie <profesor_floretu@op.pl>
2025-07-16 14:44:43 +0200

    Pireus questionnaire translated.

--- scripts/translate_greek.py
+++ scripts/translate_greek.py
@@ -2,7 +2,18 @@
 from eupolis import RAW, PROC
 import pandas as pd
 from eupolis.utils import rename_columns, strip_string
-from eupolis.translation import WEARABLES_A, QUESTIONNAIRE_Q, QUESTIONNAIRE_A
+from eupolis.translation import (
+    WEARABLES_A,
+    QUESTIONNAIRE_Q,
+    QUESTIONNAIRE_A,
+    QUALITY_A,
+    DEPRESION_A,
+    PEACE_A,
+    CRIME_A,
+    ENCOURAGEMENT_A,
+    MAINTENANCE_A,
+    OTHER_A,
+)
 import re
 
 # %%
@@ -25,6 +36,13 @@ def translate_questionnaire():
         if isinstance(x, str)
         else x
     )
+    df.iloc[:, 35:42] = df.iloc[:, 35:42].map(lambda x: QUALITY_A.get(x, x))
+    df.iloc[:, 42:63] = df.iloc[:, 42:63].map(lambda x: DEPRESION_A.get(x, x))
+    df.iloc[:, 63:66] = df.iloc[:, 63:66].map(lambda x: PEACE_A.get(x, x))
+    df.iloc[:, 66:68] = df.iloc[:, 66:68].map(lambda x: CRIME_A.get(x, x))
+    df.iloc[:, 68:73] = df.iloc[:, 68:73].map(lambda x: ENCOURAGEMENT_A.get(x, x))
+    df.iloc[:, 73:84] = df.iloc[:, 73:84].map(lambda x: MAINTENANCE_A.get(x, x))
+    df.iloc[:, 84:] = df.iloc[:, 84:].map(lambda x: OTHER_A.get(x, x))
     df.to_excel(PROC / "questionnaire_pireus.xlsx", index=False)
 
 
